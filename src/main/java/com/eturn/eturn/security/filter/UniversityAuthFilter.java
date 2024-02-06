1d671567a864a9f468f67fbfc342516ccc798ad6
v.snigerev <v.snigerev@vk.team>
2024-02-06 23:34:15 +0300

    add TODO's for Andrey

---
+++ src/main/java/com/eturn/eturn/security/filter/UniversityAuthFilter.java
@@ -0,0 +1,44 @@
+package com.eturn.eturn.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class UniversityAuthFilter extends OncePerRequestFilter {
+    private final HttpClient client = HttpClient.newHttpClient();
+
+    // feign client
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+
+        Long userId = Long.valueOf(request.getHeader("user_id"));
+        String username = request.getHeader("username");
+        String password = request.getHeader("password");
+
+//        if (client.send(HttpRequest.newBuilder().GET().build(), new )) {
+//
+////            SecurityContextHolder.getContext().setAuthentication(
+////                new Authentication(GRANT.MEMBER, userId)
+////            );
+//
+//            filterChain.doFilter(request, response);
+//        } else {
+//            SecurityContextHolder.clearContext();
+//            filterChain.doFilter(request, response);
+//        }
+    }
+
+    public enum GRANT {
+        MEMBER, ADMIN
+    }
+}
