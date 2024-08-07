0d5e5d33c8bf2ed3b56c4ef1a5f3419c4f6d772e
eturn.vasilev <andreyvasiliev1204@gmail.com>
2024-08-07 16:46:43 +0300

    updated getPositions

--- src/main/java/com/eturn/eturn/dto/mapper/PositionMoreInfoMapper.java
+++ src/main/java/com/eturn/eturn/dto/mapper/PositionMoreInfoMapper.java
@@ -21,5 +21,15 @@ public interface PositionMoreInfoMapper {
     @Mapping(target = "start", source = "position.start")
     @Mapping(target = "dateEnd", source = "position.dateEnd")
     @Mapping(target = "difference", source = "dif")
+    @Mapping(target = "isLast", ignore = true)
     PositionMoreInfoDTO positionMoreInfoToPositionDTO(Position position, int dif);
+    @Mapping(target="id", source="position.id")
+    @Mapping(target="name", source="position.user.name")
+    @Mapping(target="group", source="position.groupName")
+    @Mapping(target = "userId", source = "position.user.id")
+    @Mapping(target = "start", source = "position.start")
+    @Mapping(target = "dateEnd", source = "position.dateEnd")
+    @Mapping(target = "difference", source = "dif")
+    @Mapping(target = "isLast", source = "isLast")
+    PositionMoreInfoDTO positionMoreUserToPositionDTO(Position position, int dif, boolean isLast);
 }
